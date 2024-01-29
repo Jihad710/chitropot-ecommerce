433574b66e9ddeafe52793b0c0ced70a6b0f17e5
advertisement01 <dvfaruq@insaaftechsolutions.com>
2024-01-29 17:27:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Layout from './Components/Layout';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+   <>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Layout></Layout>}>
+    <Route index element={<Home></Home>}></Route>
+    <Route path='about' element={<About></About>}></Route>
+    <Route path='contact' element={<Contact></Contact>}></Route>
+      
+</Route>
+    </Routes>
+    </BrowserRouter>
+   </>
   );
 }
 
